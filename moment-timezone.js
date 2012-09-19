820a9fef1c9ee156f6fc7b32ba1a6dc3db17aaeb
Tim Wood <washwithcare@gmail.com>
2012-09-19 13:57:48 -0700

    Adding some tests for rulesets

--- moment-timezone.js
+++ moment-timezone.js
@@ -6,167 +6,223 @@
 
 (function () {
 
-    var moment = require('moment'),
+	var moment = require('moment'),
 
-        momentTZ,
+		momentTZ,
 
-        rules = [],
-        rulesets = [],
-        zoneRules = [],
-        zones = {};
+		rules = {},
+		rulesets = {},
+		zoneRules = [],
+		zones = {};
 
-    /************************************
-        Rules
-    ************************************/
+	/************************************
+		Rules
+	************************************/
 
-    function parseOnRule (_on) {
-        if (_on.match(/^last/)) {
-            return {
-                last: _on.split('last')[1]
-            };
-        }
-    }
+	/*
+	 * Rule
+	 *
+	 * @param _name    The string identifier for the timezone name (eg. US, NYC, Mexico...)
+	 * @param _from    The start year
+	 * @param _to      The end year
+	 * @param _type
+	 * @param _in      The month to start on
+	 * @param _on      The day to start on. A colon separated tuple of a letter and a number
+	 *                 eg. "l:1", "e:20", "f:1"
+	 *                 "l" The last on this day of week (while num > 7, skip a week)
+	 *                 "f" The first of this day of week (while num > 7, skip a week)
+	 *                 "e" This exact date of the month
+	 * @param _at      The 24 hour time of day that the switch takes place
+	 * @param _save    The number of hours to save. Usually 1 or 0
+	 * @param _letters The string to replace the ZoneRule with
+	 */
+	function Rule (_name, _from, _to, _type, _in, _on, _at, _save, _letters) {
+		this._name    = _name;
+		this._from    = +_from;
+		this._to      = +_to;
+		this._type    = _type;
+		this._month   = +_in;
 
-    /*
-     * Rule
-     *
-     * @param _name    The string identifier for the timezone name (eg. US, NYC, Mexico...)
-     * @param _from    The start year
-     * @param _to      The end year
-     * @param _type    
-     * @param _in      The month to start on
-     * @param _on      The day to start on. A colon separated tuple of a letter and a number
-                       eg. "l:1", "e:20", "f:1"
-                       "l" The last on this day of week (while num > 7, skip a week)
-                       "f" The first of this day of week (while num > 7, skip a week)
-                       "e" This exact date of the month
-     * @param _at      The 24 hour time of day that the switch takes place
-     * @param _save    The number of hours to save. Usually 1 or 0
-     * @param _letters The string to replace the ZoneRule with
-     */
-    function Rule (_name, _from, _to, _type, _in, _on, _at, _save, _letters) {
-        this._name    = _name;
-        this._from    = +_from;
-        this._to      = +_to;
-        this._type    = _type;
-        this._month   = +_in;
+		_on = _on.split(':');
+		this._dayRule = _on[0];
+		this._dayVal  = +_on[1];
 
-        _on = _on.split(':');
-        this._dayRule = _on[0];
-        this._dayVal  = +_on[1];
+		this._time    = _at;
+		this._save    = _save;
+		this._letters = _letters;
+	}
 
-        this._time    = _at;
-        this._save    = _save;
-        this._letters = _letters;
-    }
+	Rule.prototype = {
+		contains : function (mom) {
+			var year = mom.year(),
+				month = mom.month();
+			if (year < this._from || year > this._to) {
+				return false;
+			}
+			if (month < this._month || mom.date() < this._dateForYear(year)) {
+				// if it's an earlier month, use the year before
+				if (year > this._from) {
+					return true;
+				}
+				return false;
+			}
+			return true;
+		},
 
-    Rule.prototype = {
-        contains : function (mom) {
-            var year = mom.year(),
-                month = mom.month();
-            if (year < this._from || year > this._to) {
-                return false;
-            }
-            if (month < this._month) {
-                return false;
-            }
-            if (mom.date() < this._dateForYear(year)) {
-                return false;
-            }
-            return true;
-        },
+		_momentForYear : function (year) {
+			return moment([year, this._month, this._dateForYear(year)]);
+		},
 
-        _dateForYear : function (year) {
-            var firstDowOfMonth,
-                lastDowOfMonth,
-                daysInMonth,
-                day = this._dayVal,
-                dow = day % 7,
-                output;
-            switch (this._dayRule) {
-                case "l":
-                    // TODO
-                    lastDowOfMonth = moment([year, this._month + 1, 0]).day();
-                    daysInMonth = moment([year, this._month, 1]).daysInMonth();
-                    output = daysInMonth + (dow - (lastDowOfMonth - 1)) - (~~(day / 7) * 7);
-                    if (dow >= lastDowOfMonth) {
-                        output -= 7;
-                    }
-                    return output;
-                case "f":
-                    // find first day of month
-                    firstDowOfMonth = moment([year, this._month, 1]).day();
-                    output = day + 1 - firstDowOfMonth;
-                    if (dow < firstDowOfMonth) {
-                        output += 7;
-                    }
-                    return output;
-                case "e":
-                    return this._dayVal;
-            }
-            throw "Unexpected Rule._dayRule";
-        }
-    };
+		_dateForYear : function (year) {
+			var firstDowOfMonth,
+				lastDowOfMonth,
+				daysInMonth,
+				day = this._dayVal,
+				dow = day % 7,
+				output;
+			switch (this._dayRule) {
+				case "l":
+					// TODO
+					lastDowOfMonth = moment([year, this._month + 1, 0]).day();
+					daysInMonth = moment([year, this._month, 1]).daysInMonth();
+					output = daysInMonth + (dow - (lastDowOfMonth - 1)) - (~~(day / 7) * 7);
+					if (dow >= lastDowOfMonth) {
+						output -= 7;
+					}
+					return output;
+				case "f":
+					// find first day of month
+					firstDowOfMonth = moment([year, this._month, 1]).day();
+					output = day + 1 - firstDowOfMonth;
+					if (dow < firstDowOfMonth) {
+						output += 7;
+					}
+					return output;
+				case "e":
+					return this._dayVal;
+			}
+			throw "Unexpected Rule._dayRule";
+		}
+	};
 
-    /************************************
-        Rulesets
-    ************************************/
+	/************************************
+		Rulesets
+	************************************/
 
-    function Ruleset (_name) {
-        
-    }
+	function Ruleset (_name) {
+		this._name = _name;
+		this._rules = [];
+	}
 
-    ZoneRule.prototype = {
+	Ruleset.prototype = {
+		addRule : function (rule) {
+			this._rules.push(rule);
+		},
 
-    };
+		_pickRule : function (startRule, endRule, mom) {
+			var tmp,
+				start, end,
+				year = mom.year();
 
-    /************************************
-        ZoneRules
-    ************************************/
+			if (!endRule) {
+				return startRule;
+			}
 
-    function ZoneRule (_name, _offset, _rule, _format, _until) {
-        this._name = _name;
-    }
+			start = startRule._momentForYear(year);
+			end = endRule._momentForYear(year);
 
-    ZoneRule.prototype = {
+			if (start > end) {
+				tmp = start;
+				start = end;
+				end = tmp;
+			}
 
-    };
+			if (mom < start || mom > end) {
+				return endRule;
+			}
+			return startRule;
+		},
 
-    /************************************
-        Zones
-    ************************************/
+		rule : function (mom) {
+			var i, startRule, endRule;
+			for (i = 0; i < this._rules.length; i++) {
+				if (this._rules[i].contains(mom)) {
+					if (startRule) {
+						endRule = this._rules[i];
+					} else {
+						startRule = this._rules[i];
+					}
+				}
+			}
 
-    function Zone (_name) {
+			return this._pickRule(startRule, endRule, mom);
+		}
+	};
 
-    }
+	/************************************
+		ZoneRules
+	************************************/
 
-    Zone.prototype = {
+	function ZoneRule (_name, _offset, _rule, _format, _until) {
+		this._name = _name;
+	}
 
-    };
+	ZoneRule.prototype = {
 
-    /************************************
-        Global Methods
-    ************************************/
+	};
 
-    function addRules (rulesArray) {
-        var i;
-        for (i = 0; i < rulesArray.length; i++) {
-            addRule(rulesArray[i]);
-        }
-    }
+	/************************************
+		Zones
+	************************************/
 
-    function addRule (ruleString) {
-        var p = ruleString.split(',');
-            rule = new Rule(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9]);
-        rules.push(rule);
-        return rule;
-    }
+	function Zone (_name) {
 
-    module.exports = {
-        addRule : addRule,
-        addRules : addRules
-    };
+	}
 
-    addRules(require('./data/js/northamerica').rules);
+	Zone.prototype = {
+
+	};
+
+	/************************************
+		Global Methods
+	************************************/
+
+	function addRules (rulesArray) {
+		var i;
+		for (i = 0; i < rulesArray.length; i++) {
+			addRule(rulesArray[i]);
+		}
+	}
+
+	function addRule (ruleString) {
+		// don't duplicate rules
+		if (rules[ruleString]) {
+			return rules[ruleString];
+		}
+
+		var p = ruleString.split(','),
+			rule = new Rule(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9]);
+
+		rules[ruleString] = rule;
+
+		if (!rulesets[p[0]]) {
+			rulesets[p[0]] = new Ruleset(p[0]);
+		}
+		rulesets[p[0]].addRule(rule);
+
+		return rule;
+	}
+
+	function getRuleSet (name) {
+		return rulesets[name];
+	}
+
+	module.exports = {
+		addRule : addRule,
+		addRules : addRules,
+		getRuleSet : getRuleSet
+	};
+
+	// addRules(require('./data/js/northamerica').rules);
 
 }).apply(this);
---
+++ tests/core/rulesets.js
@@ -0,0 +1,40 @@
+var TZ = require("../../moment-timezone"),
+	moment = require("moment");
+
+exports.rules = {
+	"setUp" : function (cb) {
+		cb();
+	},
+
+	"contains month" : function (test) {
+		test.expect(3);
+
+		TZ.addRule("RULESET,2000,2008,,2,e:2,2:00,1:00,D");
+		TZ.addRule("RULESET,2000,2008,,6,e:3,2:00,0:00,D");
+		TZ.addRule("RULESET,2009,2010,,2,e:4,2:00,1:00,D");
+
+		var ruleset = TZ.getRuleSet('RULESET');
+
+		test.equal(ruleset.rule(moment([2007, 1, 2]))._dayVal, 3, "Ruleset should get correct rule on earlier month");
+		test.equal(ruleset.rule(moment([2007, 2, 2]))._dayVal, 2, "Ruleset should get correct rule on exact month");
+		test.equal(ruleset.rule(moment([2007, 3, 2]))._dayVal, 2, "Ruleset should get correct rule on later month");
+
+		test.done();
+	},
+
+	"contains day" : function (test) {
+		test.expect(3);
+
+		TZ.addRule("RULESET,2000,2008,,2,e:2,2:00,1:00,D");
+		TZ.addRule("RULESET,2000,2008,,6,e:3,2:00,0:00,D");
+		TZ.addRule("RULESET,2009,2010,,2,e:4,2:00,1:00,D");
+
+		var ruleset = TZ.getRuleSet('RULESET');
+
+		test.equal(ruleset.rule(moment([2007, 2, 1]))._dayVal, 3, "Ruleset should get correct rule on earlier day");
+		test.equal(ruleset.rule(moment([2007, 2, 2]))._dayVal, 2, "Ruleset should get correct rule on exact day");
+		test.equal(ruleset.rule(moment([2007, 2, 3]))._dayVal, 2, "Ruleset should get correct rule on later day");
+
+		test.done();
+	}
+};
