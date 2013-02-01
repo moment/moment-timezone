310c7c60d9cbb9ab36cdb1d84a9fbdb43437157a
Andrew Sprouse <andrew.sprouse@newsweekdailybeast.com>
2013-02-01 15:33:26 -0500

    Fixed typo in test case

--- tests/formatting/z-format-token.js
+++ tests/formatting/z-format-token.js
@@ -14,7 +14,7 @@ exports.rules = {
         test.expect(2);
 
         test.equal(moment("2011-01-01T04:59:59+00:00").tz("America/New_York").format("MMM D, YYYY h:mm A z"), "Dec 31, 2010 11:59 PM EST", "2011-01-01T04:59:59+00:00 should be Dec 31, 2010 11:59 PM EST in America/New_York");
-        test.equal(moment("2012-05-11T04:00:00+00:00").tz("America/New_York").format("MMM D, YYYY h:mm A z"), "May 11w, 2012 12:00 AM EDT", "2011-01-01T04:59:59+00:00 should be May 11, 2012 12:00 AM EDT in America/New_York");
+        test.equal(moment("2012-05-11T04:00:00+00:00").tz("America/New_York").format("MMM D, YYYY h:mm A z"), "May 11, 2012 12:00 AM EDT", "2011-01-01T04:59:59+00:00 should be May 11, 2012 12:00 AM EDT in America/New_York");
 
         test.done();
     }
