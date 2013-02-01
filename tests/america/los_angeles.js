8faae397f5774d392c751a50553f4919ab5a65a1
Andrew Sprouse <andrew.sprouse@newsweekdailybeast.com>
2013-02-01 15:18:20 -0500

    Separated out the formatting test cases so they won't get deleted by the generate-tests task.

--- tests/america/los_angeles.js
+++ tests/america/los_angeles.js
@@ -1134,14 +1134,5 @@ exports.rules = {
 		test.equal(moment("2011-11-06T09:00:00+00:00").tz("America/Los_Angeles").format("HH:mm"), "01:00", "2011-11-06T09:00:00+00:00 should be 01:00 in America/Los_Angeles");
 
 		test.done();
-	},
-
-    "America/New_York timezone format" : function (test) {
-        test.expect(2);
-
-        test.equal(moment("2011-01-01T04:59:59+00:00").tz("America/Los_Angeles").format("MMM D, YYYY h:mm A z"), "Dec 31, 2010 8:59 PM PST", "2011-01-01T04:59:59+00:00 should be Dec 31, 2010 8:59 PM PST in America/Los_Angeles");
-        test.equal(moment("2012-05-11T04:00:00+00:00").tz("America/Los_Angeles").format("MMM D, YYYY h:mm A z"), "May 10, 2012 9:00 PM PDT", "2011-01-01T04:59:59+00:00 should be May 10, 2012 9:00 PM PDT in America/Los_Angeles");
-
-        test.done();
-    }
+	}
 };
