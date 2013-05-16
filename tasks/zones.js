aee65fe9974df8327c39ebcbde1bfba1e62ae09f
Tim Wood <washwithcare@gmail.com>
2013-05-16 11:11:52 -0700

    fixing longitude parser

--- tasks/zones.js
+++ tasks/zones.js
@@ -28,11 +28,11 @@ function replacer(key, value) {
 	}, {});
 }
 
-function parseLatLong (input) {
+function parseLatLong (input, isLong) {
 	var sign = input[0] === '+' ? 1 : -1,
-		deg = ~~input.substr(1, 2) * sign,
-		min = ~~input.substr(3, 2),
-		sec = ~~input.substr(5, 2);
+		deg = ~~input.substr(1, 2 + isLong) * sign,
+		min = ~~input.substr(3 + isLong, 2),
+		sec = ~~input.substr(5 + isLong, 2);
 
 	min += sec / 60;
 	deg += min / 60;
@@ -142,8 +142,8 @@ module.exports = function (grunt) {
 			if (!this.meta[name]) {
 				this.meta[name] = {};
 			}
-			this.meta[name].lat = parseLatLong(latlon[0]);
-			this.meta[name].lon = parseLatLong(latlon[1]);
+			this.meta[name].lat = parseLatLong(latlon[0], 0);
+			this.meta[name].lon = parseLatLong(latlon[1], 1);
 		},
 
 		load : function (filename) {
