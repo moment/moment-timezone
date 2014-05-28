05458384aa57d44d36664115bf10507fbd985af4
Tim Wood <washwithcare@gmail.com>
2014-05-27 21:49:41 -0700

    Add packing of floating point numbers and tests

--- moment-timezone-utils.js
+++ moment-timezone-utils.js
@@ -2,21 +2,56 @@
 
 var BASE60 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX'.split('');
 
-function packBase60(number) {
+// Used to attempt to fix floating point rounding errors
+var EPSILON = 0.000001;
+
+function packBase60Fraction(fraction, precision) {
+	var buffer = '.',
+		output = '',
+		current;
+
+	while (precision > 0) {
+		precision  -= 1;
+		fraction   *= 60;
+		current     = Math.floor(fraction + EPSILON);
+		buffer     += BASE60[current];
+		fraction   -= current;
+
+		// Only add buffer to output once we have a non-zero value.
+		// This makes '.000' output '', and '.100' output '.1'
+		if (current) {
+			output += buffer;
+			buffer  = '';
+		}
+	}
+
+	return output;
+}
+
+function packBase60(number, precision) {
 	var output = '',
-		nextNumber;
+		absolute = Math.abs(number),
+		whole = Math.floor(absolute),
+		fraction = packBase60Fraction(absolute - whole, Math.min(~~precision, 10));
+
+	while (whole > 0) {
+		output = BASE60[whole % 60] + output;
+		whole = Math.floor(whole / 60);
+	}
 
 	if (number < 0) {
-		number = -number;
-		output = '-';
+		output = '-' + output;
 	}
 
-	while (number > 0) {
-		nextNumber = Math.floor(number / 60);
-		output = BASE60[(number - (60 * nextNumber))] + output;
-		number = nextNumber;
+	if (output && fraction) {
+		return output + fraction;
 	}
-	return output || '0';
+
+	if (!fraction && output === '-') {
+		return '0';
+	}
+
+	return output || fraction || '0';
 }
 
 module.exports = {
