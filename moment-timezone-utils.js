c4a18ebe4e39da35f023f9eff8567c0edd94c4d5
Tim Wood <washwithcare@gmail.com>
2014-05-28 08:00:22 -0700

    Add tests for packing bad input data

--- moment-timezone-utils.js
+++ moment-timezone-utils.js
@@ -96,7 +96,21 @@ function packAbbrsAndOffsets(source) {
 	return abbrs.join(' ') + '|' + offsets.join(' ') + '|' + indices.join('');
 }
 
+function validatePackData (source) {
+	if (!source.name)    { throw new Error("Missing name"); }
+	if (!source.abbrs)   { throw new Error("Missing abbrs"); }
+	if (!source.untils)  { throw new Error("Missing untils"); }
+	if (!source.offsets) { throw new Error("Missing offsets"); }
+	if (
+		source.offsets.length !== source.untils.length ||
+		source.offsets.length !== source.abbrs.length
+	) {
+		throw new Error("Mismatched array lengths");
+	}
+}
+
 function pack (source) {
+	validatePackData(source);
 	return source.name + '|' + packAbbrsAndOffsets(source) + '|' + packUntils(source.untils);
 }
 
--- tests/moment-timezone/pack.js
+++ tests/moment-timezone/pack.js
@@ -3,7 +3,7 @@
 var tz = require("../../moment-timezone-utils");
 
 function compare(test, source, expected) {
-	test.equal(tz.pack(source), expected, 'should be able to pack name');
+	test.equal(tz.pack(source), expected, 'should be able to pack data');
 
 	test.done();
 }
@@ -26,5 +26,77 @@ exports.pack = {
 			},
 			"Some/Zone_Name|ABC DEF GHI|10 3X 10|010121|-1aX 20 3X 20 3X"
 		);
+	},
+
+	errors : function (test) {
+		test.throws(function () {
+			tz.pack({
+				abbrs : ['ABC'],
+				offsets : [60],
+				untils : [1000]
+			});
+		}, "Packing data without a name should throw an error");
+
+		test.throws(function () {
+			tz.pack({
+				name : "",
+				abbrs : ['ABC'],
+				offsets : [60],
+				untils : [1000]
+			});
+		}, "Packing data without a name should throw an error");
+
+		test.throws(function () {
+			tz.pack({
+				name : "Test/Name",
+				offsets : [60],
+				untils : [1000]
+			});
+		}, "Packing data without abbrs should throw an error");
+
+		test.throws(function () {
+			tz.pack({
+				name : "Test/Name",
+				abbrs : ["ABC"],
+				untils : [1000]
+			});
+		}, "Packing data without offsets should throw an error");
+
+		test.throws(function () {
+			tz.pack({
+				name : "Test/Name",
+				abbrs : ["ABC"],
+				offsets : [60]
+			});
+		}, "Packing data without untils should throw an error");
+
+		test.throws(function () {
+			tz.pack({
+				name : "Test/Name",
+				abbrs : ['ABC'],
+				offsets : [60, 20],
+				untils : [1000]
+			});
+		}, "Packing data with mismatched offset length should throw an error");
+
+		test.throws(function () {
+			tz.pack({
+				name : "Test/Name",
+				abbrs : ['ABC', 'DEF'],
+				offsets : [60],
+				untils : [1000]
+			});
+		}, "Packing data with mismatched abbr length should throw an error");
+
+		test.throws(function () {
+			tz.pack({
+				name : "Test/Name",
+				abbrs : ['ABC'],
+				offsets : [60],
+				untils : [1000, 2000]
+			});
+		}, "Packing data with mismatched untils length should throw an error");
+
+		test.done();
 	}
 };
