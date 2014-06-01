0978876b09a297266a64cfa15dce2e507c71ff5a
Tim Wood <washwithcare@gmail.com>
2014-06-01 10:25:47 -0700

    Fix download link for non-latest releases.

--- tasks/data-download.js
+++ tasks/data-download.js
@@ -8,12 +8,18 @@ module.exports = function (grunt) {
 		version = version || 'latest';
 
 		var done  = this.async(),
-			src   = 'ftp://ftp.iana.org/tz/tzdata-' + version + '.tar.gz',
+			src   = 'ftp://ftp.iana.org/tz/tzdata-latest.tar.gz',
 			curl  = path.resolve('temp/curl', version, 'data.tar.gz'),
 			dest  = path.resolve('temp/download', version);
 
+		if (version !== 'latest') {
+			src = 'http://www.iana.org/time-zones/repository/releases/tzdata' + version + '.tar.gz';
+		}
+
 		grunt.file.mkdir(path.dirname(curl));
 		grunt.file.mkdir(dest);
+
+		grunt.log.ok('Downloading ' + src);
 
 		exec('curl ' + src + ' -o ' + curl + ' && cd ' + dest + ' && gzip -dc ' + curl + ' | tar -xf -', function (err) {
 			if (err) { throw err; }
