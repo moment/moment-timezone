75671cad9b80591acb065204cf141bb6540d75e1
Tim Wood <washwithcare@gmail.com>
2014-06-30 12:58:51 -0700

    Add failing tests for moving invalid input times forward.

--- tests/moment-timezone/parse.js
+++ tests/moment-timezone/parse.js
@@ -11,7 +11,9 @@ exports.parse = {
 		done();
 	},
 
-	"ambiguous input losing an hour - America/Los_Angeles" : function (t) {
+	"invalid input - moveInvalidForward : false - Los Angeles" : function (t) {
+		moment.tz.moveInvalidForward = false;
+
 		// the hour from 2am to 3am does not exist on March 11 2011 in America/Los_Angeles
 		var before  = moment.tz([2012, 2, 11, 1, 59, 59], "America/Los_Angeles"),
 			atStart = moment.tz([2012, 2, 11, 2, 0, 0],   "America/Los_Angeles"),
@@ -19,19 +21,21 @@ exports.parse = {
 			after   = moment.tz([2012, 2, 11, 3, 0, 0],   "America/Los_Angeles");
 
 		t.equal( before.format("HH mm ss Z"), "01 59 59 -08:00", "Before the lost hour, the time should match the input time");
-		t.equal(atStart.format("HH mm ss Z"), "01 00 00 -08:00", "During the lost hour, the time should fall back to the previous time");
-		t.equal(  atEnd.format("HH mm ss Z"), "01 59 59 -08:00", "During the lost hour, the time should fall back to the previous time");
-		t.equal(  after.format("HH mm ss Z"), "03 00 00 -07:00", "After the lost hour, the time should match the input time");
+		t.equal(atStart.format("HH mm ss Z"), "01 00 00 -08:00", "During the lost hour, the time should roll back to the previous time");
+		t.equal(  atEnd.format("HH mm ss Z"), "01 59 59 -08:00", "During the lost hour, the time should roll back to the previous time");
+		t.equal(  after.format("HH mm ss Z"), "03 00 00 -07:00",  "After the lost hour, the time should match the input time");
 
 		t.equal( before.zone(), 480, "Before the lost hour, the offset should match the non-dst offset");
 		t.equal(atStart.zone(), 480, "During the lost hour, the offset should match the non-dst offset");
 		t.equal(  atEnd.zone(), 480, "During the lost hour, the offset should match the non-dst offset");
-		t.equal(  after.zone(), 420, "After the lost hour, the time should match the dst offset");
+		t.equal(  after.zone(), 420,  "After the lost hour, the offset should match the dst offset");
 
 		t.done();
 	},
 
-	"ambiguous input losing an hour - America/New_York" : function (t) {
+	"invalid input - moveInvalidForward : false - New York" : function (t) {
+		moment.tz.moveInvalidForward = false;
+
 		// the hour from 2am to 3am does not exist on March 11 2011 in America/New_York
 		var before  = moment.tz([2012, 2, 11, 1, 59, 59], "America/New_York"),
 			atStart = moment.tz([2012, 2, 11, 2, 0, 0], "America/New_York"),
@@ -39,14 +43,58 @@ exports.parse = {
 			after   = moment.tz([2012, 2, 11, 3, 0, 0], "America/New_York");
 
 		t.equal( before.format("HH mm ss Z"), "01 59 59 -05:00", "Before the lost hour, the time should match the input time");
-		t.equal(atStart.format("HH mm ss Z"), "01 00 00 -05:00", "During the lost hour, the time should fall back to the previous time");
-		t.equal(  atEnd.format("HH mm ss Z"), "01 59 59 -05:00", "During the lost hour, the time should fall back to the previous time");
-		t.equal(  after.format("HH mm ss Z"), "03 00 00 -04:00", "After the lost hour, the time should match the input time");
+		t.equal(atStart.format("HH mm ss Z"), "01 00 00 -05:00", "During the lost hour, the time should roll back to the previous time");
+		t.equal(  atEnd.format("HH mm ss Z"), "01 59 59 -05:00", "During the lost hour, the time should roll back to the previous time");
+		t.equal(  after.format("HH mm ss Z"), "03 00 00 -04:00",  "After the lost hour, the time should match the input time");
 
 		t.equal( before.zone(), 300, "Before the lost hour, the offset should match the non-dst offset");
 		t.equal(atStart.zone(), 300, "During the lost hour, the offset should match the non-dst offset");
 		t.equal(  atEnd.zone(), 300, "During the lost hour, the offset should match the non-dst offset");
-		t.equal(  after.zone(), 240, "After the lost hour, the time should match the dst offset");
+		t.equal(  after.zone(), 240,  "After the lost hour, the offset should match the dst offset");
+
+		t.done();
+	},
+
+	"invalid input - moveInvalidForward : true - Los Angeles" : function (t) {
+		moment.tz.moveInvalidForward = true;
+
+		// the hour from 2am to 3am does not exist on March 11 2011 in America/Los_Angeles
+		var before  = moment.tz([2012, 2, 11, 1, 59, 59], "America/Los_Angeles"),
+			atStart = moment.tz([2012, 2, 11, 2, 0, 0],   "America/Los_Angeles"),
+			atEnd   = moment.tz([2012, 2, 11, 2, 59, 59], "America/Los_Angeles"),
+			after   = moment.tz([2012, 2, 11, 3, 0, 0],   "America/Los_Angeles");
+
+		t.equal( before.format("HH mm ss Z"), "01 59 59 -08:00", "Before the lost hour, the time should match the input time");
+		t.equal(atStart.format("HH mm ss Z"), "03 00 00 -07:00", "During the lost hour, the time should roll forward to the previous time");
+		t.equal(  atEnd.format("HH mm ss Z"), "03 59 59 -07:00", "During the lost hour, the time should roll forward to the previous time");
+		t.equal(  after.format("HH mm ss Z"), "03 00 00 -07:00",  "After the lost hour, the time should match the input time");
+
+		t.equal( before.zone(), 480, "Before the lost hour, the offset should match the non-dst offset");
+		t.equal(atStart.zone(), 420, "During the lost hour, the offset should match the dst offset");
+		t.equal(  atEnd.zone(), 420, "During the lost hour, the offset should match the dst offset");
+		t.equal(  after.zone(), 420,  "After the lost hour, the offset should match the dst offset");
+
+		t.done();
+	},
+
+	"invalid input - moveInvalidForward : true - New York" : function (t) {
+		moment.tz.moveInvalidForward = true;
+
+		// the hour from 2am to 3am does not exist on March 11 2011 in America/New_York
+		var before  = moment.tz([2012, 2, 11, 1, 59, 59], "America/New_York"),
+			atStart = moment.tz([2012, 2, 11, 2, 0, 0], "America/New_York"),
+			atEnd   = moment.tz([2012, 2, 11, 2, 59, 59], "America/New_York"),
+			after   = moment.tz([2012, 2, 11, 3, 0, 0], "America/New_York");
+
+		t.equal( before.format("HH mm ss Z"), "01 59 59 -05:00", "Before the lost hour, the time should match the input time");
+		t.equal(atStart.format("HH mm ss Z"), "03 00 00 -04:00", "During the lost hour, the time should roll forward to the previous time");
+		t.equal(  atEnd.format("HH mm ss Z"), "03 59 59 -04:00", "During the lost hour, the time should roll forward to the previous time");
+		t.equal(  after.format("HH mm ss Z"), "03 00 00 -04:00",  "After the lost hour, the time should match the input time");
+
+		t.equal( before.zone(), 300, "Before the lost hour, the offset should match the non-dst offset");
+		t.equal(atStart.zone(), 240, "During the lost hour, the offset should match the dst offset");
+		t.equal(  atEnd.zone(), 240, "During the lost hour, the offset should match the dst offset");
+		t.equal(  after.zone(), 240,  "After the lost hour, the offset should match the dst offset");
 
 		t.done();
 	},
