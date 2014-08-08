e29b603a99af8c0d1dc8657c73e7d6ba86219564
Patrick Hogan <askhogan@gmail.com>
2014-08-08 03:23:36 -0700

    Add in Tim's suggestions - cleanup one console.error
    I added your suggestions.  I checked through your old versions and it appears that moment.version has been around for awhile.  But since the suggestion came from the original author I wasn't surprised!
    
    I also changed one other area that was using the old console.error instead of logEntry
    
    I kept my if check at the bottom to check for if(momentProperties) because this cleans up the uncaught exception

--- moment-timezone.js
+++ moment-timezone.js
@@ -25,6 +25,14 @@
 		zones = {},
 		links = {};
 
+	// Moment.js version check
+	var momentVersion = moment.version.split('.');
+	var major = +momentVersion[0];
+	var minor = +momentVersion[1];
+	if (major < 2 || (major === 2 && minor < 6)) {
+		logError('Moment Timezone requires Moment.js >= ' + major + '.' + minor + '. You are using Moment.js ' + moment.version + '. See momentjs.com');
+	}
+
 	/************************************
 		Unpacking
 	************************************/
@@ -274,9 +282,7 @@
 	function zoneExists (name) {
 		if (!zoneExists.didShowError) {
 			zoneExists.didShowError = true;
-			if (typeof console !== 'undefined' && typeof console.error === 'function') {
-				console.error("moment.tz.zoneExists('" + name + "') has been deprecated in favor of !moment.tz.zone('" + name + "')");
-			}
+				logError("moment.tz.zoneExists('" + name + "') has been deprecated in favor of !moment.tz.zone('" + name + "')");
 		}
 		return !!getZone(name);
 	}
@@ -386,9 +392,6 @@
 	} else if(momentProperties) {
 		// moment 2.7.0
 		momentProperties._z = null;
-	} else{
-		// moment < 2.6 - unsupported
-		console.error("moment-timezone.js: Your version of moment is unsupported.  moment-timezone requires moment.js 2.6+ go to http://momentjs.com to get the newest version.");
 	}
 
 	// INJECT DATA
