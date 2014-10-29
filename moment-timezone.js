a40ca33a074b4ebadd1b2494c3a9c95d853d6821
David Moravek <david.moravek@gmail.com>
2014-10-29 17:27:34 +0100

    make sure normalizeName argument is a string

--- moment-timezone.js
+++ moment-timezone.js
@@ -193,7 +193,9 @@
 	************************************/
 
 	function normalizeName (name) {
-		return (name || '').toLowerCase().replace(/\//g, '_');
+		if (typeof name !== 'string')
+			name = '';
+		return name.toLowerCase().replace(/\//g, '_');
 	}
 
 	function addZone (packed) {
