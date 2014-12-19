26289da41f8ef4897ba1e4f254f0c96f023b8fcb
Tim Wood <washwithcare@gmail.com>
2014-12-19 10:36:48 -0800

    Add temporary wrapper for moment#utcOffset until it lands in moment core

--- tests/moment-timezone/utc.js
+++ tests/moment-timezone/utc.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var moment = require("../../");
+var helpers = require("../helpers/helpers");
 
 exports.utc = {
 	utc : function (test) {
@@ -11,34 +12,34 @@ exports.utc = {
 
 		var m = moment("2014-07-10 12:00:00+00:00"),
 			localFormat = m.format(),
-			localOffset = m.utcOffset();
+			localOffset = helpers.getUTCOffset(m);
 
 		m.tz("TestUTC/Pacific");
 
-		test.equal(m.utcOffset(), -480, "Should change the offset when using moment.fn.tz");
+		test.equal(helpers.getUTCOffset(m), -480, "Should change the offset when using moment.fn.tz");
 		test.equal(m.format(), "2014-07-10T04:00:00-08:00", "Should change the offset when using moment.fn.tz");
 
 		m.utc();
 		moment.updateOffset(m);
 
-		test.equal(m.utcOffset(), 0, "Should set the offset to +00:00 when using moment.fn.utc");
+		test.equal(helpers.getUTCOffset(m), 0, "Should set the offset to +00:00 when using moment.fn.utc");
 		test.equal(m.format(), "2014-07-10T12:00:00+00:00", "Should change the offset when using moment.fn.utc");
 
 		m.tz("TestUTC/Eastern");
 
-		test.equal(m.utcOffset(), -300, "Should change the offset when using moment.fn.tz");
+		test.equal(helpers.getUTCOffset(m), -300, "Should change the offset when using moment.fn.tz");
 		test.equal(m.format(), "2014-07-10T07:00:00-05:00", "Should change the offset when using moment.fn.tz");
 
 		m.utc();
 		moment.updateOffset(m);
 
-		test.equal(m.utcOffset(), 0, "Should set the offset to +00:00 when using moment.fn.utc");
+		test.equal(helpers.getUTCOffset(m), 0, "Should set the offset to +00:00 when using moment.fn.utc");
 		test.equal(m.format(), "2014-07-10T12:00:00+00:00", "Should change the offset when using moment.fn.utc");
 
 		m.local();
 		moment.updateOffset(m);
 
-		test.equal(m.utcOffset(), localOffset, "Should reset the offset to local time when using moment.fn.local");
+		test.equal(helpers.getUTCOffset(m), localOffset, "Should reset the offset to local time when using moment.fn.local");
 		test.equal(m.format(), localFormat, "Should reset the offset to local time when using moment.fn.local");
 
 		test.done();
