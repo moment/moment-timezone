577d4865cc9ecb72d3c2f9c5ea8d6bb93f8c7e4c
Tim Wood <washwithcare@gmail.com>
2016-03-09 08:54:36 -0600

    [ES6] grunt data-zic task

--- tasks/data-zic.js
+++ tasks/data-zic.js
@@ -1,31 +1,27 @@
-"use strict";
+import path from 'path';
+import { exec } from 'child_process';
 
-var path = require('path'),
-	exec = require('child_process').exec;
-
-module.exports = function (grunt) {
-	grunt.registerTask('data-zic', '2. Compile data sources with zic(8).', function (version) {
-		version = version || 'latest';
-
-		var done  = this.async(),
-			dest  = path.resolve('temp/zic', version),
-			files = 'africa antarctica asia australasia etcetera europe northamerica southamerica pacificnew backward'.split(' ');
+export default grunt => {
+	grunt.registerTask('data-zic', '2. Compile data sources with zic(8).', function (version = 'latest') {
+		const done  = this.async();
+		const dest  = path.resolve('temp/zic', version);
+		const files = 'africa antarctica asia australasia etcetera europe northamerica southamerica pacificnew backward'.split(' ');
 
 		grunt.file.mkdir(dest);
 
 		function next () {
 			if (!files.length) {
-				grunt.log.ok('Compiled zic for ' + version);
+				grunt.log.ok(`Compiled zic for ${ version }`);
 				return done();
 			}
 
-			var file = files.shift(),
-				src = path.resolve('temp/download', version, file);
+			const file = files.shift();
+			const src = path.resolve('temp/download', version, file);
 
-			exec('zic -d ' + dest + ' ' + src, function (err) {
+			exec(`zic -d ${ dest } ${src }`, err => {
 				if (err) { throw err; }
 
-				grunt.verbose.ok('Compiled zic ' + version + ':' + file);
+				grunt.verbose.ok(`Compiled zic ${ version }:${ file }`);
 
 				next();
 			});
