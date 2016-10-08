5a4f7a32f70484c3ea1815c6b1b1725677c43112
ssskip <reducecode@gmail.com>
2016-10-08 13:01:33 +0800

    update to  IANA 2016g data

--- tests/zones/indian/kerguelen.js
+++ tests/zones/indian/kerguelen.js
@@ -3,13 +3,11 @@
 var helpers = require("../../helpers/helpers");
 
 exports["Indian/Kerguelen"] = {
-	"guess" : helpers.makeTestGuess("Indian/Kerguelen", { offset: false, abbr: true }),
-
 	"1949" : helpers.makeTestYear("Indian/Kerguelen", [
 		["1949-12-31T23:59:59+00:00", "23:59:59", "-00", 0]
 	]),
 
 	"1950" : helpers.makeTestYear("Indian/Kerguelen", [
-		["1950-01-01T00:00:00+00:00", "05:00:00", "TFT", -300]
+		["1950-01-01T00:00:00+00:00", "05:00:00", "+05", -300]
 	])
 };
