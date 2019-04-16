9e3a9b864e679a3565c7a09bda3272f847712bbb
Matt Johnson <mattjoh@microsoft.com>
2019-04-15 23:10:51 -0700

    Fix failing test

--- tests/zones/etc/uct.js
+++ tests/zones/etc/uct.js
@@ -6,7 +6,7 @@ exports["Etc/UCT"] = {
 
 	"guess:by:offset" : helpers.makeTestGuess("Etc/UCT", { offset: true, expect: "Africa/Abidjan" }),
 
-	"guess:by:abbr" : helpers.makeTestGuess("Etc/UCT", { abbr: true }),
+	"guess:by:abbr" : helpers.makeTestGuess("Etc/UCT", { abbr: true, expect: "Etc/UTC" }),
 
 
 };
