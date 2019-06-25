da5af2455e2ab31bee58debcc63bfeacefa98fa2
Daniel Ruf <daniel@daniel-ruf.de>
2019-06-26 00:45:04 +0200

    fix: stabilize Array.sort

--- moment-timezone.js
+++ moment-timezone.js
@@ -292,7 +292,10 @@
 		if (a.abbrScore !== b.abbrScore) {
 			return a.abbrScore - b.abbrScore;
 		}
-		return b.zone.population - a.zone.population;
+		if (a.zone.population !== b.zone.population) {
+			return b.zone.population - a.zone.population;
+		}
+		return b.zone.name.localeCompare(a.zone.name);
 	}
 
 	function addToGuesses (name, offsets) {
