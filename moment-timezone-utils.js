fe733a151b3c63ae777fe7ebde7f1cef52aad4ba
ellenaua <elenasharovar@gmail.com>
2020-05-16 18:13:21 +0300

    Release 0.5.30 - IANA 2020a

--- moment-timezone-utils.js
+++ moment-timezone-utils.js
@@ -1,5 +1,5 @@
 //! moment-timezone-utils.js
-//! version : 0.5.29
+//! version : 0.5.30
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
