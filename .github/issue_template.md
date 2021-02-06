5d8b9e3505985b5f229f8ba0c7cae804bba8b1f1
Elena Sharovar <elenasharovar@gmail.com>
2021-02-06 09:04:51 +0200

    2021a (#928)
    * Create issue_template.md
    
    * 2021a (#927)
    
    * 2021a
    
    * 2021a

---
+++ .github/issue_template.md
@@ -0,0 +1,8 @@
+### Moment-timezone version which you use:
+
+Version:
+
+_Note: many issues are resolved if you just upgrade to the latest version_
+
+### Issue description:
+
--- tests/zones/africa/brazzaville.js
+++ tests/zones/africa/brazzaville.js
@@ -8,8 +8,23 @@ exports["Africa/Brazzaville"] = {
 
 	"guess:by:abbr" : helpers.makeTestGuess("Africa/Brazzaville", { abbr: true, expect: "Africa/Lagos" }),
 
+	"1905" : helpers.makeTestYear("Africa/Brazzaville", [
+		["1905-06-30T23:46:24+00:00", "23:59:59", "LMT", -815 / 60],
+		["1905-06-30T23:46:25+00:00", "23:46:25", "GMT", 0]
+	]),
+
+	"1908" : helpers.makeTestYear("Africa/Brazzaville", [
+		["1908-06-30T23:59:59+00:00", "23:59:59", "GMT", 0],
+		["1908-07-01T00:00:00+00:00", "00:13:35", "LMT", -815 / 60]
+	]),
+
+	"1913" : helpers.makeTestYear("Africa/Brazzaville", [
+		["1913-12-31T23:46:24+00:00", "23:59:59", "LMT", -815 / 60],
+		["1913-12-31T23:46:25+00:00", "00:16:25", "+0030", -30]
+	]),
+
 	"1919" : helpers.makeTestYear("Africa/Brazzaville", [
-		["1919-08-31T23:46:23+00:00", "23:59:59", "LMT", -816 / 60],
-		["1919-08-31T23:46:24+00:00", "00:46:24", "WAT", -60]
+		["1919-08-31T23:29:59+00:00", "23:59:59", "+0030", -30],
+		["1919-08-31T23:30:00+00:00", "00:30:00", "WAT", -60]
 	])
 };
