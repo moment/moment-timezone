2f22dffdf544c6c735a437b453afcabb5ddfe8da
ellenaua <elenasharovar@gmail.com>
2021-11-09 17:39:01 +0200

    2021a

--- moment-timezone.js
+++ moment-timezone.js
@@ -1,5 +1,5 @@
 //! moment-timezone.js
-//! version : 0.5.33
+//! version : 0.5.34
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
@@ -29,7 +29,7 @@
 	// 	return moment;
 	// }
 
-	var VERSION = "0.5.33",
+	var VERSION = "0.5.34",
 		zones = {},
 		links = {},
 		countries = {},
--- tests/zones/pacific/enderbury.js
+++ tests/zones/pacific/enderbury.js
@@ -8,6 +8,11 @@ exports["Pacific/Enderbury"] = {
 
 	"guess:by:abbr" : helpers.makeTestGuess("Pacific/Enderbury", { abbr: true, expect: "Pacific/Tongatapu" }),
 
+	"1937" : helpers.makeTestYear("Pacific/Enderbury", [
+		["1937-08-30T23:59:59+00:00", "23:59:59", "-00", 0],
+		["1937-08-31T00:00:00+00:00", "12:00:00", "-12", 720]
+	]),
+
 	"1979" : helpers.makeTestYear("Pacific/Enderbury", [
 		["1979-10-01T11:59:59+00:00", "23:59:59", "-12", 720],
 		["1979-10-01T12:00:00+00:00", "01:00:00", "-11", 660]
