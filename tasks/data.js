e551fdef071e59eb4b2b9d34012edd7cd60ae824
Iskren Chernev <me@iskren.info>
2022-08-25 10:44:37 +0300

    data: allow running pipeline without wiping temp
    Data pipeline ran with separate versions use different folders, so it
    makes sense to allow running multiple data pipelines without wiping in
    between.
    
    The default is still to wipe clean, so nobody will get caught by
    surprise.

--- tasks/data.js
+++ tasks/data.js
@@ -4,7 +4,9 @@ module.exports = function (grunt) {
 	grunt.registerTask('data', 'Build timezone data.', function (version) {
 		version = version || 'latest';
 
-		grunt.task.run('clean:data');
+		if (!grunt.option('dirty-run')) {
+			grunt.task.run('clean:data');
+		}
 
 		grunt.task.run([
 			'data-download:' + version,
