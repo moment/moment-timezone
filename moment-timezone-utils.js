ac6de03cf34610068185961613d719bc050c7d2b
Iskren Chernev <me@iskren.info>
2022-08-25 14:34:47 +0300

    Bump version to 0.5.36

--- moment-timezone-utils.js
+++ moment-timezone-utils.js
@@ -1,5 +1,5 @@
 //! moment-timezone-utils.js
-//! version : 0.5.35
+//! version : 0.5.36
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
--- moment-timezone.js
+++ moment-timezone.js
@@ -1,5 +1,5 @@
 //! moment-timezone.js
-//! version : 0.5.35
+//! version : 0.5.36
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
@@ -29,7 +29,7 @@
 	// 	return moment;
 	// }
 
-	var VERSION = "0.5.35",
+	var VERSION = "0.5.36",
 		zones = {},
 		links = {},
 		countries = {},
