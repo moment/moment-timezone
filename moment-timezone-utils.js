f1619a73d371ef712a6dd44421364657cf34548a
Gilmore Davidson <gilmoreorless@gmail.com>
2022-12-11 21:01:55 +1100

    Bump version to 0.5.40

--- moment-timezone-utils.js
+++ moment-timezone-utils.js
@@ -1,5 +1,5 @@
 //! moment-timezone-utils.js
-//! version : 0.5.39
+//! version : 0.5.40
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
--- moment-timezone.js
+++ moment-timezone.js
@@ -1,5 +1,5 @@
 //! moment-timezone.js
-//! version : 0.5.39
+//! version : 0.5.40
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
@@ -29,7 +29,7 @@
 	// 	return moment;
 	// }
 
-	var VERSION = "0.5.39",
+	var VERSION = "0.5.40",
 		zones = {},
 		links = {},
 		countries = {},
